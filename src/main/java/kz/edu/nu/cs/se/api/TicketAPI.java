e1cdc1c498b44f5d05e7e27cd8db28d4a2739cd1
Madina Ryspayeva <madinaryspayeva@MacBook-Air-Madina-3.local>
2019-10-17 17:10:39 +0600

    my ticket api for now and front in index.html

--- src/main/java/kz/edu/nu/cs/se/api/TicketAPI.java
+++ src/main/java/kz/edu/nu/cs/se/api/TicketAPI.java
@@ -12,18 +12,32 @@ import java.sql.Statement;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-@Path("/ticket")
+@Path("/tickets")
 public class TicketAPI {
 
     @GET
-    @Path("{id: [0-9]+}")
+    @Path("{user_id: [0-9]+}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getUserTickets(@PathParam("id") String id) {
+    //public Response getUserTickets(@PathParam("user_id") String id) {
+    public Response getUserTickets(@PathParam("user_id") String id,
+    								@DefaultValue("all") @QueryParam("state") String state) {	
         Gson gson = new Gson();
         try {
             // TODO : Check if id exists at the first place
             Connection conn = DBConnector.getDatabaseConnection();
-            String sql = "SELECT * FROM `railways-service`.`ticket`WHERE `user_id` = " + id;
+            String sql;
+            //state = 5 - future; state = 0 past
+            if(state == "future")
+            	sql = "SELECT * FROM ticket, trip WHERE trip.id = ticket.trip_id and "
+            			+ "datediff(now(), trip.departure_time) < 0 and user_id = " + id
+            			+ " ORDER BY trip.departure_time";
+            else if(state == "past")
+            	sql = "SELECT * FROM ticket, trip WHERE trip.id = ticket.trip_id and "
+        			+ "datediff(now(), trip.departure_time) > 0 and user_id = " + id
+        			+ " ORDER BY trip.departure_time";
+            else sql = "SELECT * FROM ticket, trip WHERE trip.id = ticket.trip_id and "
+            		+ "user_id = " + id
+            		+ " ORDER BY trip.departure_time";
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
 
@@ -53,9 +67,11 @@ public class TicketAPI {
         }
         return Response.ok(gson.toJson(new String("error"))).build();
     }
+    
+    
 
     @POST
-    @Path("{id: [0-9]+}")
+    @Path("{user_id: [0-9]+}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response createUserTickets(@PathParam("id") String id, String body) {
@@ -68,6 +84,45 @@ public class TicketAPI {
 
         System.out.println(t.getId());
         return Response.ok(gson.toJson(new String("test"))).build();
+    }
+    
+    @GET
+    @Path("{user_id: [0-9]+}/{ticket_id: [0-9]+}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getThisTicket(@PathParam("user_id") String user_id,
+    								@PathParam("ticket_id") String ticket_id) {
+    	Gson gson = new Gson();
+    	
+    	try {
+            // TODO : Check if id exists at the first place
+            Connection conn = DBConnector.getDatabaseConnection();
+            String sql = "SELECT * FROM ticket WHERE user_id = " + user_id + " and id = " + ticket_id;
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            rs.next();
+            
+            Ticket ticket = new Ticket();
+
+            ticket.setId(rs.getInt("id"));
+            ticket.setUserId(rs.getInt("user_id"));
+            ticket.setTripId(rs.getInt("trip_id"));
+            ticket.setSeatId(rs.getInt("seat_id"));
+            ticket.setPrice(rs.getDouble("price"));
+            ticket.setStatus(rs.getString("status"));
+            
+
+            rs.close();
+            stmt.close();
+            conn.close();
+
+            return Response.ok(gson.toJson(ticket)).build();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    	
+    	return Response.ok(gson.toJson(new String("error"))).build();
     }
 
 
