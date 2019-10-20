40ae08e66384d7d0fdce9fcfd6538b037cecfc73
talgatomarov <talgat.omarov.1999@gmail.com>
2019-10-20 12:08:53 +0600

    Fixed authentication in tickets page

--- src/main/java/kz/edu/nu/cs/se/web/TicketServlet.java
+++ src/main/java/kz/edu/nu/cs/se/web/TicketServlet.java
@@ -1,11 +1,14 @@
 package kz.edu.nu.cs.se.web;
 
+import kz.edu.nu.cs.se.security.Auth0JwtPrincipal;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.Principal;
 
 // Supposed to be smth like /profile/tickets but waiting for profile
 @WebServlet(urlPatterns = {"/tickets"})
@@ -17,6 +20,13 @@ public class TicketServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Principal principal = request.getUserPrincipal();
+
+        if (principal instanceof Auth0JwtPrincipal) {
+            Auth0JwtPrincipal auth0JwtPrincipal = (Auth0JwtPrincipal) principal;
+            request.setAttribute("profile", auth0JwtPrincipal.getIdToken().getClaims());
+        }
+
         String id = request.getParameter("id");
         if (id == null) {
             request.getRequestDispatcher("views/ticketsTable.jsp").forward(request, response);
