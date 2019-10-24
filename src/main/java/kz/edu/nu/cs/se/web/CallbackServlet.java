77f5fdb9fcbd78d19b37b7354c537b2d28524293
talgatomarov <talgat.omarov.1999@gmail.com>
2019-10-24 20:09:18 +0600

    Retrieved user id and fixed profile Referer

--- src/main/java/kz/edu/nu/cs/se/web/CallbackServlet.java
+++ src/main/java/kz/edu/nu/cs/se/web/CallbackServlet.java
@@ -5,9 +5,12 @@ import com.auth0.jwt.interfaces.Claim;
 import kz.edu.nu.cs.se.ConfiguredSessionFactory;
 import kz.edu.nu.cs.se.models.entities.UserEntity;
 import kz.edu.nu.cs.se.security.Auth0JwtPrincipal;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import javax.persistence.PersistenceException;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,36 +26,51 @@ import java.util.Map;
 public class CallbackServlet extends HttpServlet {
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Principal principal = request.getUserPrincipal();
 
         if (principal instanceof Auth0JwtPrincipal) {
             Auth0JwtPrincipal auth0JwtPrincipal = (Auth0JwtPrincipal) principal;
             Map<String, Claim> claims = auth0JwtPrincipal.getIdToken().getClaims();
 
+            Integer userId = null;
+
             Session session = ConfiguredSessionFactory.getSession();
 
-            try {
-                session.beginTransaction();
+            Query query = session.createQuery("from UserEntity u where u.email = :email ");
+            query.setParameter("email", claims.get("email").asString());
 
-                UserEntity user = new UserEntity();
-                user.setEmail(claims.get("email").asString());
-                user.setFirstname(claims.get("given_name").asString());
-                user.setSurname(claims.get("family_name").asString());
+            UserEntity user = (UserEntity) query.uniqueResult();
 
-                session.saveOrUpdate(user);
+            if (user == null) {
+                try {
+                    session.beginTransaction();
 
-                session.getTransaction().commit();
-            } catch (PersistenceException e) {
-                // Ignore constraint violation
-            } finally {
-                session.close();
+                    user = new UserEntity();
+
+                    user.setEmail(claims.get("email").asString());
+                    user.setFirstname(claims.get("given_name").asString());
+                    user.setSurname(claims.get("family_name").asString());
+
+                    userId = (Integer) session.save(user);
+
+                    session.getTransaction().commit();
+                } catch (HibernateException e) {
+                    session.getTransaction().rollback();
+                    e.printStackTrace();
+                } finally {
+                    session.close();
+                }
+            } else {
+                userId = user.getId();
             }
+
+            request.getSession().setAttribute("userId", userId);
         }
 
         String referer = (String) request.getSession().getAttribute("Referer");
-        String redirectTo = referer != null ? referer : request.getContextPath();
+        String redirectTo = referer != null ? referer : "/";
 
-        response.sendRedirect(redirectTo);
+        request.getRequestDispatcher(redirectTo).forward(request, response);
     }
 }
