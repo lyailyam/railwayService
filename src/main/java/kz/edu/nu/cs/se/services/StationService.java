6b606135da4ad9cdfc9c794ae83db761a6412302
talgatomarov <talgat.omarov.1999@gmail.com>
2019-10-31 18:32:36 +0600

    Added POST /stations method

--- src/main/java/kz/edu/nu/cs/se/services/StationService.java
+++ src/main/java/kz/edu/nu/cs/se/services/StationService.java
@@ -1,11 +1,11 @@
 package kz.edu.nu.cs.se.services;
 
 import kz.edu.nu.cs.se.ConfiguredSessionFactory;
+import kz.edu.nu.cs.se.models.entities.StationEntity;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -25,6 +25,32 @@ public class StationService {
         session.getTransaction().commit();
         session.close();
 
+        if (result.isEmpty()) {
+            return Response.status(404).build();
+        }
+
         return Response.ok().entity(result).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addStation(StationEntity station) {
+        Session session = ConfiguredSessionFactory.getSession();
+
+        try {
+            session.beginTransaction();
+
+            session.save(station);
+
+            session.getTransaction().commit();
+        } catch (HibernateException e)
+        {
+            session.getTransaction().rollback();
+            e.printStackTrace();
+            return Response.status(400).build();
+        } finally {
+            session.close();
+        }
+        return Response.ok().build();
     }
 }
