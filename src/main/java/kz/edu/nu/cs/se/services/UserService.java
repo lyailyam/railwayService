bab58e4cf661cf3609445f7e6d43726a0d680da5
talgatomarov <talgat.omarov.1999@gmail.com>
2019-10-31 16:26:57 +0600

    Changed context.xml and UserService

--- src/main/java/kz/edu/nu/cs/se/services/UserService.java
+++ src/main/java/kz/edu/nu/cs/se/services/UserService.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class UserService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getTickets() {
+    public Response getUsers() {
         List result = null;
 
         Session session = ConfiguredSessionFactory.getSession();
