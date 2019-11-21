bde21a45964e1b1870a40f7385833fc9e693de77
talgatomarov <talgat.omarov.1999@gmail.com>
2019-11-21 17:52:05 +0600

    Added userId

--- src/main/java/kz/edu/nu/cs/se/ResponseLoggingFilter.java
+++ src/main/java/kz/edu/nu/cs/se/ResponseLoggingFilter.java
@@ -1,15 +1,27 @@
 package kz.edu.nu.cs.se;
 
+import org.jboss.weld.context.http.Http;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.ext.Provider;
 
 @Logged
 @Provider
 public class ResponseLoggingFilter implements ContainerResponseFilter {
+    @Context
+    HttpServletRequest webRequest;
+
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        final HttpSession session = webRequest.getSession();
+
+        Integer userId = (Integer) session.getAttribute("userId");
+        System.out.println(userId);
         System.out.println(java.time.LocalTime.now());
         System.out.println(requestContext.getMethod());
         System.out.println(requestContext.getUriInfo().getAbsolutePath().toASCIIString());
