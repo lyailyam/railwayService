3ecf4ebc78f7e48816617e502a36f55292cda4c0
talgatomarov <talgat.omarov.1999@gmail.com>
2019-11-22 14:54:54 +0600

    Changed AdvisoryService

--- src/main/java/kz/edu/nu/cs/se/services/AdvisoryService.java
+++ src/main/java/kz/edu/nu/cs/se/services/AdvisoryService.java
@@ -1,21 +1,20 @@
 package kz.edu.nu.cs.se.services;
 
 
-import com.google.gson.Gson;
-import kz.edu.nu.cs.se.DBConnector;
-import kz.edu.nu.cs.se.models.*;
+import kz.edu.nu.cs.se.SessionFactoryListener;
+import kz.edu.nu.cs.se.models.entities.AdvisoryEntity;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 @Path("/advisories")
 public class AdvisoryService {
@@ -23,64 +22,59 @@ public class AdvisoryService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAdvisories() {
+        List result = null;
 
-        String advisoriesSql = "select * from advisory order by date_created, time_created ";
-
-        Gson gson = new Gson();
-        List<AdvisoryMessage> messagesList = new CopyOnWriteArrayList<>();
-
+        Session session = SessionFactoryListener.getSession();
         try {
-            Connection conn = DBConnector.getDatabaseConnection();
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(advisoriesSql);
+            session.beginTransaction();
 
-            while(rs.next()) {
+            Query query = session.createQuery("from AdvisoryEntity ");
 
-                AdvisoryMessage adv = new AdvisoryMessage();
+            result = query.list();
 
-                adv.setManagerId(rs.getInt("manager_id"));
-                adv.setMessage(rs.getString("adv_message"));
-                adv.setDateCreated(rs.getDate("date_created").toString());
-                adv.setTimeCreated(rs.getTime("time_created").toString());
+            session.getTransaction().commit();
 
-                messagesList.add(adv);
-
-            }
-
-
-            conn.close();
-            return Response.ok(gson.toJson(messagesList)).build();
-        } catch (SQLException e) {
+        } catch (HibernateException e) {
+            session.getTransaction().rollback();
             e.printStackTrace();
-            return Response.status(404).build();
-        } catch (Exception e) {
-            return Response.status(500).build();
+            return Response.status(400).build();
+        } finally {
+            session.close();
         }
 
-
+        return Response.ok().entity(result).build();
     }
 
-    /*@POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response createAdvisoryMessage(AdvisoryMessage advisory) {
-        String sql = "INSERT INTO advisory (manager_id, adv_message, date_created, time_created) "
-                + String.format("VALUES (%s,'%s','%s','%s')", advisory.getManagerId(), advisory.getMessage(),
-                ticket.getSeatId(), ticket.getTripId(), ticket.getStatus());
+    static void cancelRouteMessage(Long routeId, String date) {
+        String message = "Route " + routeId + " on " + date + " was cancelled.";
+        addMessage(message);
+    }
 
-        try(Connection conn = DBConnector.getDatabaseConnection();
-//            Statement stmt = conn.createStatement()) {
-//            int rs = stmt.executeUpdate(sql);
-//            return Response.status(rs).build();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        // TODO: Return a proper return with proper error status and message
-//        return Response.ok("test").build();
+    static void updateRouteMessage(Long routeId, String date) {
+        String message = "Route " + routeId + " on " + date + " was updated. Please, check your ticket";
+        addMessage(message);
+    }
 
+    private static void addMessage(String message) {
+        Timestamp timestamp = new Timestamp(new Date().getTime());
+        AdvisoryEntity entity = new AdvisoryEntity();
+        entity.setDatetime(timestamp);
 
-    }*/
+        Session session = SessionFactoryListener.getSession();
 
+        entity.setMessage(message);
+        try {
+            session.beginTransaction();
 
+            session.save(entity);
 
-
+            session.getTransaction().commit();
+        } catch (HibernateException e)
+        {
+            session.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
 }
