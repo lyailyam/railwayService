a836c459697f5162037ea56872b582b51c8dd1a6
talgatomarov <talgat.omarov.1999@gmail.com>
2019-11-22 12:09:05 +0600

    Added LogService

---
+++ src/main/java/kz/edu/nu/cs/se/services/LogService.java
@@ -0,0 +1,83 @@
+package kz.edu.nu.cs.se.services;
+
+import kz.edu.nu.cs.se.SessionFactoryListener;
+import kz.edu.nu.cs.se.logging.Logged;
+import kz.edu.nu.cs.se.models.entities.SeatEntity;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/logs")
+public class LogService {
+
+    @Path("/api")
+    @GET
+    @Logged
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getApiLogs() {
+
+        List result = null;
+
+        Session session = SessionFactoryListener.getSession();
+        try {
+            session.beginTransaction();
+
+            Query query = session.createQuery("from LogApiEntity");
+
+            result = query.list();
+
+            session.getTransaction().commit();
+
+        } catch (HibernateException e) {
+            session.getTransaction().rollback();
+            e.printStackTrace();
+            return Response.status(400).build();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            e.printStackTrace();
+            return Response.status(500).build();
+        } finally {
+            session.close();
+        }
+
+        return Response.ok().entity(result).build();
+    }
+
+
+    @Path("/users")
+    @GET
+    @Logged
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUserLogs() {
+
+        List result = null;
+
+        Session session = SessionFactoryListener.getSession();
+        try {
+            session.beginTransaction();
+
+            Query query = session.createQuery("from LogUserEntity ");
+
+            result = query.list();
+
+            session.getTransaction().commit();
+
+        } catch (HibernateException e) {
+            session.getTransaction().rollback();
+            e.printStackTrace();
+            return Response.status(400).build();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            e.printStackTrace();
+            return Response.status(500).build();
+        } finally {
+            session.close();
+        }
+        return Response.ok().entity(result).build();
+    }
+}
